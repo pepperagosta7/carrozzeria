8ecdc99a770140eca7a8620ff418ae8a82ea749e
pepperagosta7 <giusepperagosta29@gmail.com>
2025-03-11 14:47:24 +0100

    first commit

---
+++ src/main/java/com/example/carrozzeria/CarrozzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrozzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrozzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrozzeriaApplication.class, args);
+	}
+
+}
